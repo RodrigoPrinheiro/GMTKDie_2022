137f3d401a9cce3cfb21f2dc19cdae5e0f79d170
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2022-07-16 20:31:49 +0100

    Dice mapping

--- Assets/Game/Scripts/Die/DieFaces.cs
+++ Assets/Game/Scripts/Die/DieFaces.cs
@@ -1,9 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class DieFaces : MonoBehaviour
 {
+    public enum Direction
+    {
+        Zp = 0,
+        Zm = 1,
+        Yp = 2,
+        Ym = 3,
+        Xp = 4,
+        Xm = 5
+    }
+    private Dictionary<Direction, Transform> diceSidesDic;
+    private void Awake() 
+    {
+        diceSidesDic = new Dictionary<Direction, Transform>();
+        
+        foreach(Transform child in transform)
+        {
+            Direction d = Enum.Parse<Direction>(child.name);
+            diceSidesDic.Add(d, child);
+        }
+    }
+
+    public void SetFace(DiceGameEvent e, Direction dir)
+    {
+        // Generate face
+    }
+
+    public Transform GetTransform(Direction dir)
+    {
+        return diceSidesDic[dir];
+    }
+
     private void OnDrawGizmos() {
         foreach(Transform c in transform)
         {
--- Assets/Game/Scripts/Die/TheDie.cs
+++ Assets/Game/Scripts/Die/TheDie.cs
@@ -2,17 +2,102 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class SideChoice
+{
+    public Transform faceTransform;
+    public DiceGameEvent diceSideEvent;
+}
+
 public class TheDie : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private DieFaces faces;
+    private Animator animator;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float rotateDuration = 4f;
+    [SerializeField] private float choiceRotationDuration = 0.6f;
+    [SerializeField] private AnimationCurve speedIncreaseCurve;
+    private int rollsCount;
+    private Dictionary<DieFaces.Direction, DiceGameEvent> diceState;
+
+    private void Awake() 
     {
-        
+        faces = GetComponentInChildren<DieFaces>();
+        animator = GetComponent<Animator>();
+
+        diceState = new Dictionary<DieFaces.Direction, DiceGameEvent>();
+    }
+    
+    private void Start() 
+    {
+        rollsCount = 0;
+        RandomizeFaces(0);
+        Roll();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Roll()
     {
+        animator.SetTrigger("StartRoll");
+        StartCoroutine(RollAndPick());
+    }
+
+    public void RandomizeFaces(int rolls)
+    {
+        var pickedEvents = GameData.instance.EventsForRollCount(rolls);
+        int index = 0;
+        foreach (var item in pickedEvents)
+        {
+            index++;
+            DieFaces.Direction d = (DieFaces.Direction)index;
+            
+            if (!diceState.ContainsKey(d))
+                diceState.Add(d, item);
+            else
+                diceState[d] = item;
+            
+            faces.SetFace(item, d);
+        }
+    }
+
+    public SideChoice GetRandomChoice()
+    {
+        DieFaces.Direction dir = (DieFaces.Direction)Random.Range(0, 6);
+        DiceGameEvent picked = diceState[dir];
         
+        return new SideChoice(){faceTransform = faces.GetTransform(dir), diceSideEvent = picked};
+    }
+
+    public IEnumerator RollAndPick()
+    {
+        SideChoice choice = GetRandomChoice();
+
+        float elapsed = 0f;
+        float newRotationTimer = 0f;
+        Vector3 target = Random.insideUnitSphere.normalized;
+        while(elapsed <= rotateDuration)
+        {
+            if (newRotationTimer >= 1f)
+            {
+                target = Random.insideUnitSphere.normalized;
+                newRotationTimer = 0;
+            }
+            float speed = rotationSpeed * speedIncreaseCurve.Evaluate(elapsed / rotateDuration);
+            transform.Rotate(target * Time.deltaTime * speed);
+
+            elapsed += Time.deltaTime;
+            newRotationTimer += Time.deltaTime;
+            yield return null;
+        }
+
+
+        elapsed = 0f;
+        while(elapsed <= choiceRotationDuration)
+        {
+            float singleStep = rotationSpeed * Time.deltaTime;
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        animator.SetTrigger("StopRoll");
     }
 }
