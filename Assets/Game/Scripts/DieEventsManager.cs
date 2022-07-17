c53b507392fd892ec7ae139d1f6680a0f26c5b29
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2022-07-17 16:41:14 +0100

    fix animation on dice and fixed persistent event timing

--- Assets/Game/Scripts/DieEventsManager.cs
+++ Assets/Game/Scripts/DieEventsManager.cs
@@ -19,7 +19,7 @@ public class DieEventsManager : MonoBehaviour
     private void Update()
     {
         if (EventRunning) return;
-
+        
         if (queuedGameEvents.Count > 0)
         {
             EventInstance e = queuedGameEvents.Dequeue();
@@ -31,7 +31,6 @@ public class DieEventsManager : MonoBehaviour
         {
             if (activePersistentEvents[i].CanTrigger())
             {
-                Debug.Log("Triggered persistent event");
                 activePersistentEvents[i].Run();
             }
         }
