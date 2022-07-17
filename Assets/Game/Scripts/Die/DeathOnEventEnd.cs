ac177b94a273b20178da4939283b7788d6396e64
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2022-07-17 19:24:38 +0100

    Death on end

---
+++ Assets/Game/Scripts/Die/DeathOnEventEnd.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathOnEventEnd : MonoBehaviour
+{
+    private void OnDestroy()
+    {
+        int completed = PlayerPrefs.GetInt("Game Completed", 0);
+        completed++;
+        PlayerPrefs.SetInt("Game Completed", completed);
+        Player.instance.Die();
+    }
+}
